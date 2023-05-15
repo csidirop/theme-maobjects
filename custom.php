350a63b51855c1fb007d534ffd4e9733d332b7ff
Jörg Mechnich <joerg.mechnich@uni-mannheim.de>
2023-05-15 20:59:55 +0200

    Add anchor to item URL using a filter instead of hardcoding

--- custom.php
+++ custom.php
@@ -1,3 +1,13 @@
 <?php
 
 require_once dirname(__FILE__) . '/functions.php';
+
+add_filter(array('Display', 'Item', 'Item Type Metadata', 'URL'), 'make_url_link');
+
+function make_url_link($url)
+{
+    if(empty($url)) {
+        return;
+    }
+    return '<a href="'.$url.'" target="_blank">'.$url.'</a>';
+}
