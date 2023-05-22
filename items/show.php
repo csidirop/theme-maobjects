ec3347fd1a852f7068a4af3213b937e2d49d3e4e
Jörg Mechnich <joerg.mechnich@uni-mannheim.de>
2023-05-22 14:01:07 +0200

    Show item navigation on top and bottom

--- items/show.php
+++ items/show.php
@@ -6,6 +6,13 @@ echo head(array('title' => metadata('item', array('Dublin Core', 'Title')), 'bod
 
 <h1><?php echo metadata('item', 'rich_title', array('no_escape' => true)); ?></h1>
 
+<nav>
+<ul class="item-pagination navigation">
+    <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+    <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+</ul>
+</nav>
+
 <?php
 if ($itemFiles) {
     $image = item_image('thumbnail', array(), 0, $item);
