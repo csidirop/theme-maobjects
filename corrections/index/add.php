815e995f91fb0b895c3eac911f87decc3d6ff97c
Christos Sidiropoulos <csidirop@runbox.com>
2024-03-28 15:25:25 +0000

    Refactor the keyword suggestion form (PlugIn Corrections)
    Signed-off-by: Christos Sidiropoulos <csidirop@runbox.com>

--- corrections/index/add.php
+++ corrections/index/add.php
@@ -16,62 +16,43 @@
     <?php echo __('Please enter your suggestions in the field below. Separate multiple keywords with commas. An administrator will review your contribution.'); ?>
 </p>
 
+<style>
+    /* TODO: move everting to left or center and add 'Current data' to the table */
+    .element-correction .explanation {
+        line-height: 20px;
+        font-size: 1rem;
+    }
+    .element-correction button{
+        display: none;
+    }
+    .element-correction .correction-current-data {
+        font-size: 1rem;
+        color: #666;
+        text-align: right;
+        border-top: 0px;
+        margin-right: 1rem;
+    }
+</style>
 
 <form method='post'>
-<div class="field">
-        <div class="inputs five columns omega">
-            <div class="input-block">
-                <textarea cols='50' rows='3' name='comment'></textarea>
-            </div>
-        </div>
-    </div>
-
-    <?php if (!$user) : ?>
-        <div class="field">
-            <div class="two columns alpha">
-                <label for='email'><?php echo __('Email'); ?></label>
-            </div>
-            <div class="inputs five columns omega">
-                <p class="explanation"></p>
-                <div class="input-block">
-                    <input type='text' name='email' />
-                </div>
-            </div>
-        </div>
-
-    <?php endif; ?>
-
-    <div class="field">
-        <div>
-            <label for='may_contact' style='display: inline'><?php echo __('Can we contact you?'); ?></label>
-            <div class="input-inline" style='display: inline; margin-left: 20px'>
-                <input type='checkbox' value='1' name='may_contact' />
-            </div>
-        </div>
-    </div>
-
-    <!-- we dont need this -->
-    <!-- <?php
+    <?php
         foreach ($elements as $element) {
             echo "<div class='element-correction' >";
             $elName = $element->name;
-            $elSet = $element->getElementSet();
-            $elSetName = $elSet->name;
+            $elSetName = $element->getElementSet()->name;
             echo $this->elementForm($element, $corrections_correction);
-            echo "<p class='correction-current-data'>" . __('Current data for %s', $elName) . "</p>";
-            echo "<p>" . metadata($item, array($elSetName, $elName), array('no_filter' => true)) . "</p>";
+            echo "<p class='correction-current-data'>" . __('Current entry for %s', $elName) .": ". metadata($item, array($elSetName, $elName), array('no_filter' => true)) . "</p>";
             echo "</div>";
         }
-    ?> -->
+    ?>
 
     <?php
-    
-    echo __('<p><br><i>Thank you for taking the time to improve this site!</i></p>');
+        echo __('<p><br><i>Thank you for taking the time to improve this site!</i></p>');
 
-    if (!$user) {
-        echo $captchaScript;
-    }
-    echo $this->formSubmit('submit', __('Submit Suggestion'));
+        if (!$user) {
+            echo $captchaScript;
+        }
+        echo $this->formSubmit('submit', __('Submit Suggestion'));
     ?>
 
     <input type='hidden' name='item_id' value='<?php echo $item->id; ?>' />
