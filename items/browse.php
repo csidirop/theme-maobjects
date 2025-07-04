ffc0036c6c6be9f06bf771c2c80e632b9c1107f1
Christos Sidiropoulos <dev@csidirop.de>
2025-07-04 16:47:19 +0200

    Update the optional listview in the browse item view
    Signed-off-by: Christos Sidiropoulos <dev@csidirop.de>

--- items/browse.php
+++ items/browse.php
@@ -27,29 +27,52 @@ $sortLinks[__('Date Added')] = 'added';
 <?php endif; ?>
 
 <div class="records">
-    <?php $liststyle = (get_theme_option('browse_list_style') === 'list') ? 'list' : '' ?>
     <?php foreach (loop('items') as $item): ?>
-    <div class="item hentry <?php echo $liststyle; ?>" >
-        <div class="item-meta">
-        <?php if (metadata('item', 'has files')): ?>
-        <div class="item-img">
-            <?php echo link_to_item(item_image()); ?>
-        </div>
+        <?php if (get_theme_option('browse_list_style') === 'list'): ?>
+            <div class="item-meta hentry list">
+                <h3><?php echo link_to_item(metadata($item, array('Dublin Core', 'Title'), array('class'=>'permalink'))); ?></h3>
+
+                <div class="item-meta-content" style="display: flex;">
+                    <?php if (metadata($item, 'has thumbnail')): ?>
+                        <div class="item-img" style="margin-right: 25px;">
+                        <?php echo link_to_item(item_image()); ?>
+                        </div>
+                    <?php endif; ?>
+
+                    <span class="item-meta-details">
+                        <?php if ($creator = metadata('item', array('Dublin Core', 'Creator'))): ?>
+                            <span class="item-creator"><?php echo $creator; ?></span>
+                        <?php endif; ?>
+                        <?php if ($date = metadata('item', array('Dublin Core', 'Date'))): ?>
+                            <span class="item-date"><?php echo $date; ?></span>
+                        <?php endif; ?>
+                        <?php if ($description = metadata($item, array('Dublin Core', 'Description'), array('snippet'=>350))): ?>
+                            <div class="item-description"><?php echo $description; ?></div>
+                        <?php endif; ?>
+                    </span>
+                </div>
+            </div>
+        <?php else: ?>
+            <div class="item hentry" >
+                <div class="item-meta">
+                    <?php if (metadata('item', 'has files')): ?>
+                        <div class="item-img"> <?php echo link_to_item(item_image()); ?> </div>
+                    <?php endif; ?>
+
+                    <h2><?php echo link_to_item(null, array('class'=>'permalink')); ?></h2>
+
+                    <span class="item-meta-details">
+                        <?php if ($creator = metadata('item', array('Dublin Core', 'Creator'))): ?>
+                            <span class="item-creator"> <?php echo $creator; ?> </span>
+                        <?php endif; ?>
+                        <?php if ($date = metadata('item', array('Dublin Core', 'Date'))): ?>
+                            <span class="item-date"><?php echo $date; ?></span>
+                        <?php endif; ?>
+                    </span>
+                    <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
+                </div>
+            </div>
         <?php endif; ?>
-
-        <h2><?php echo link_to_item(null, array('class'=>'permalink')); ?></h2>
-
-        <?php if ($creator = metadata('item', array('Dublin Core', 'Creator'))): ?>
-        <span class="creator"><?php echo $creator; ?></span>
-        <?php endif; ?>
-        <?php if ($date = metadata('item', array('Dublin Core', 'Date'))): ?>
-        <span class="date"><?php echo $date; ?></span>
-        <?php endif; ?>
-
-        <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
-
-        </div><!-- end class="item-meta" -->
-    </div><!-- end class="item hentry" -->
     <?php endforeach; ?>
 </div>
 
