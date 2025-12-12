0f500af74882ae6e17cae6ad7d5f57211f32ad0b
Kim Nguyen <design@kimisgold.com>
2025-12-12 16:08:08 -0500

    Remove unused slide title id attributes.
    It's currently hard to predict the slide number from this partial with
    how the featured record display now works with `display_records()`.
    We could revisit this later if this is useful for an aria label.

--- common/featured.php
+++ common/featured.php
@@ -12,9 +12,9 @@ if ($backgroundFile) {
         <span class="sr-only"><?php echo __('Featured'); ?></span>
         <?php if ($recordType == 'Exhibit'): ?>
             <?php set_current_record('exhibit', $record); ?>
-            <span id="slick-slide0<?php echo $slideCount; ?>" class="resource-title"><?php echo link_to_exhibit(); ?></span>
+            <span class="resource-title"><?php echo link_to_exhibit(); ?></span>
         <?php else: ?>
-            <span id="slick-slide0<?php echo $slideCount; ?>" class="resource-title"><?php echo link_to($record, 'show', metadata($record, 'display_title')); ?></span>
+            <span class="resource-title"><?php echo link_to($record, 'show', metadata($record, 'display_title')); ?></span>
             <?php if ($creator = metadata($record, array('Dublin Core', 'Creator'))): ?>
             <span class="creator"><?php echo $creator; ?></span>
             <?php endif; ?>
